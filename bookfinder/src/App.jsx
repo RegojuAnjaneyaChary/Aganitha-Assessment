ca887308fb07b5ed1cb6a76d538184e96c61e5b9
RegojuAnjaneyaChary <anjaneyacharyregoju@gmail.com>
2025-09-15 13:13:11 +0530

    import correct2

--- bookfinder/src/App.jsx
+++ bookfinder/src/App.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect, useRef } from "react";
 import useDebounce from "./hooks/UseDebounce";
 
-import SearchBar from "/src/components/SearchBar";
-import BookCard from "/src/components/BookCard";
-import Spinner from "/src/components/Spinner";
+import SearchBar from "/src/components/SearchBar.jsx";
+import BookCard from "/src/components/BookCard.jsx";
+import Spinner from "/src/components/Spinner.jsx";
 
 export default function App() {
   const [query, setQuery] = useState("");
