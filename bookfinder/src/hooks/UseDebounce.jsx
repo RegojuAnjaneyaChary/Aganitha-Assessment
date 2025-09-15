36de3ca74810765d055d0384396af13bdea89975
RegojuAnjaneyaChary <anjaneyacharyregoju@gmail.com>
2025-09-15 11:34:01 +0530

    import correct commit

--- bookfinder/src/hooks/UseDebounce.jsx
+++ bookfinder/src/hooks/UseDebounce.jsx
@@ -1,5 +1,20 @@
 
 
+// import { useState, useEffect } from "react";
+
+// export default function useDebounce(value, delay = 300) {
+//   const [debounced, setDebounced] = useState(value);
+
+//   useEffect(() => {
+//     const timer = setTimeout(() => setDebounced(value), delay);
+//     return () => clearTimeout(timer);
+//   }, [value, delay]);
+
+//   return debounced;
+// }
+
+
+
 import { useState, useEffect } from "react";
 
 export default function useDebounce(value, delay = 300) {
